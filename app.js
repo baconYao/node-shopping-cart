93082088ed3cbb8af9ba7ff7b3fa8d79e6de95e0
baconYao <bacon735392@gmail.com>
2017-04-12 17:38:17 +0800

    Route Grouping & Protection

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const flash = require('connect-flash');
 const validator = require('express-validator');
 
 const index = require('./routes/index');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -35,6 +36,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  next();
+});
+
+app.use('/user', userRoutes);
 app.use('/', index);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var csrf = require('csurf');
-var passport = require('passport');
 
 var Product = require('../models/product');
 
@@ -20,32 +19,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/user/signup', (req, res, next) => {
-  var messages = req.flash('error');
-  res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
-});
 
-router.post('/user/signup',  passport.authenticate('local.signup', {
-  successRedirect: '/user/profile',
-  failureRedirect: '/user/signup',
-  failureFlash: true
-}));
-
-router.get('/user/profile', function(req, res, next) {
-  res.render('user/profile');
-});
-
-
-router.get('/user/signin', (req, res, next) => {
-  var messages = req.flash('error');
-  res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
-});
-
-router.post('/user/signin', passport.authenticate('local.signin', {
-  successRedirect: '/user/profile',
-  failureRedirect: '/user/signin',
-  failureFlash: true
-}));
 
 module.exports = router;
  
