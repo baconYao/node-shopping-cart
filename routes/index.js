9de2d20c44f4208a76c5a0d56ab18380ccfa1639
baconYao <bacon735392@gmail.com>
2017-04-14 10:35:26 +0800

    Add cart view

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,15 @@ router.get('/add-to-cart/:id', (req, res) => {
   });
 });
 
+router.get('/shopping-cart', function(req, res) {
+  if(!req.session.cart) {
+    return res.render('shop/shopping-cart', {products: null});
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart', {
+    products: cart.generateArray(), totalPrice: cart.totalPrice
+  });
+});
 
 module.exports = router;
  
