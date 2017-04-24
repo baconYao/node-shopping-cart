5400f61050644cf45c6586a38fe1011ffec1c649
baconYao <bacon735392@gmail.com>
2017-04-24 17:59:12 +0800

    Making Charges with Stripe

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,14 @@ router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var successMsg = req.flash('success')[0];
   Product.find((err, docs) => {
     var productChunks = [];
     var chunkSize = 3;
     for(var i = 0; i < docs.length; i += chunkSize) {
       productChunks.push(docs.slice(i, i+chunkSize));   //[[],[],[],...]
     }
-    res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunks, successMsg: successMsg, noMessages: !successMsg});
   });
 });
 
@@ -50,7 +51,34 @@ router.get('/checkout', function(req, res, next) {
     return res.redirect('/shopping-cart');
   }
   var cart = new Cart(req.session.cart);
-  res.render('shop/checkout', {total: cart.totalPrice, csrfToken: req.csrfToken()});
+  var errMsg = req.flash('error')[0];
+  res.render('shop/checkout', {total: cart.totalPrice, csrfToken: req.csrfToken(), errMsg: errMsg, noError: !errMsg});
+});
+
+router.post('/checkout', function(req, res, next) {
+  if(!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+
+  var stripe = require("stripe")(
+    "sk_test_MoN95w5bRJZL7THcQpDFYbkh"
+  );
+
+  stripe.charges.create({
+    amount: cart.totalPrice * 100,
+    currency: "usd",
+    source: req.body.stripeToken, // obtained with Stripe.js
+    description: "Charge for daniel.thompson@example.com"
+  }, function(err, charge) {
+      if(err) {
+        req.flash('error', err.message);
+        return res.redirect('/checkout');
+      }
+      req.flash('success', 'Successfully bought product!');
+      req.session.cart = null;
+      res.redirect('/');
+  });
 });
 
 module.exports = router;
